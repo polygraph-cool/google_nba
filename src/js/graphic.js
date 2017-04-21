6919ff67c69878a69e8ba53115affcbcfaa4349b
russellgoldenberg <russellgoldenberg@gmail.com>
2017-04-21 12:47:56 -0400

    no user select and trim search

--- src/js/graphic.js
+++ src/js/graphic.js
@@ -297,7 +297,7 @@ function resetCategory() {
 }
 
 function handleSearchChange() {
-	let name = this.value ? this.value.toLowerCase().replace(/]W/g, '') : ''
+	let name = this.value ? this.value.toLowerCase().replace(/\W/g, '') : ''
 	name = name.length > 2 ? name : null
 
 	if (name) {
@@ -310,7 +310,7 @@ function handleSearchChange() {
 		.filter(d => d.players.toLowerCase().includes(name))
 		.classed('is-player', true)
 
-	const filtered = dataFlat.filter(d => d.players.toLowerCase().replace(/]W/g, '').includes(name))
+	const filtered = dataFlat.filter(d => d.players.toLowerCase().replace(/\W/g, '').includes(name))
 
 	const byDecade = d3.nest()
 		.key(d => d.decade)
